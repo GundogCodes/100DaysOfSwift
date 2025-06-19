4646806bba2bae6e6a23893034bb17191531c785
Gunish Sharma <gunishsharma20@gmail.com>
2025-06-19 16:16:04 -0600

    day1 done

---
+++ floats.playground/Contents.swift
@@ -0,0 +1,22 @@
+//FLOATS
+
+import Cocoa
+
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+print(c)
+
+var g = 3.14
+
+g = 3
+
+print("g: ",g)
+
+var rating = 5.0
+
+rating *= 2
+print(rating)
---
+++ integers.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Cocoa
+
+let score = 10
+
+let reallyBigNum = 100_000_000
+
+let lowerScore = score - 2
+
+let higerScore = score + 10
+
+let doubleScore = score * 2
+
+let squaredScore = score * score
+
+let halvedScore = score / 2
+
+var counter = 10
+
+counter += 5
+
+print(counter)
+
+counter *= 2
+
+print(counter)
+
+counter -= 10
+
+print(counter)
+
+counter /= 2
+
+print(counter)
