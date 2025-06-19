39c74fb55d5d350c6341cbb3f227e27d12ce95f5
Gunish Sharma <gunishsharma20@gmail.com>
2025-06-19 15:46:32 -0600

    completed strings

---
+++ strings.playground/Contents.swift
@@ -0,0 +1,38 @@
+//STRINGS
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+
+let dogBreed = "Samoyed"
+
+let meaningOfLife = "How many roads must a man walk down"
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+let myName = "My name is \"Gunish\"😊"
+
+print(myName)
+print(myName.count)
+print(myName.uppercased())
+
+
