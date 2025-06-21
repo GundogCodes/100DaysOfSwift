652eed4b0676608ae780327b0f958aea25220142
Gunish Sharma <gunishsharma@Gunishs-MacBook-Pro.local>
2025-06-20 22:46:38 -0600

    done sets

--- day3/sets.playground/Contents.swift
+++ day3/sets.playground/Contents.swift
@@ -1,4 +1,9 @@
 //SETS
 import Cocoa
+var emptySet = Set<String>()
+var actors = Set(["Denzel Washington", "Nicholas Cage", "Tom Cruise"])
+actors.insert( "Tom Hanks" )
+print(actors)
+print(actors.sorted())
+print(actors.contains("Tom Cruise"))
 
-var greeting = "Hello, playground"
